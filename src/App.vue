0e838872bf1daec676125aa19e60d6c1ee33c170
Frank Garcia <garcia1901l@gmail.com>
2020-10-04 19:44:58 -0500

    upload image

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">    
+    <HelloWorld :idactivity="id" :titulo="portada1" :tipo="tipo1" :cantidad="2"></HelloWorld>
   </div>
 </template>
 
@@ -12,7 +12,14 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      id : 13,
+      portada1 : 'Titulo',
+      tipo1 : 1,
+    }
+  },
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <vue-dropzone ref="myVueDropzone" :options="dropzoneOptions" v-on:vdropzone-sending="sendingEvent" v-on:vdropzone-removed-file="deleteImage" :id="titulo"></vue-dropzone>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import vue2Dropzone from 'vue2-dropzone'
+    import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+    export default {
+        name: "Upload",
+        props: ['titulo', 'tipo','cantidad','idactivity'],
+        components: {
+            vueDropzone: vue2Dropzone
+        },
+        mounted() {
+
+        },
+        data() {
+            return {
+                user_id: '1',
+                direccion2: '',
+                portada1: '',
+                dropzoneOptions: {
+                    url: 'https://httpbin.org/post',
+                    thumbnailWidth: 150,
+                    maxFilesize: 5.5,
+                    maxFiles: this.cantidad,
+                    headers: {
+                       // "X-CSRF-TOKEN": document.head.querySelector("[name=csrf-token]").content,
+                    },
+                    addRemoveLinks: true
+                    //addRemoveLinks: true
+                },
+                titulo_galeria: '',
+                images : []
+            }
+        },
+        methods: {
+            sendingEvent (file, xhr, formData) {
+                //formData.append('user_id', this.user_id);
+                formData.append('activity_id', this.idactivity);
+                formData.append('tipo', this.tipo);
+                formData.append('titulo', this.titulo);
+            },
+            deleteImage(file) {
+                const xhrJSON = JSON.parse(file.xhr.response);
+                this.axios
+                    .delete('/uploads', {
+                        data: {
+                            imageName: xhrJSON.filename,
+                        },
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            },
+        },
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
